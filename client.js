c67a1a1484d5527d041c98f19a3f22441594be97
AndreaBruniBRUN <131588971+AndreaBruniBRUN@users.noreply.github.com>
2023-11-06 00:23:42 +0100

    test004

--- client.js
+++ client.js
@@ -5,10 +5,10 @@
 //const indirizzoIPDelServer = 'https://andreabrunibrun.github.io/Testprova/CLIENT-WEB';
 //const eventSource = new EventSource(`${indirizzoIPDelServer}/stream-dati.html`);
 //https://www.dropbox.com/s/XXXXXXXXXXXX/stream-dati.html?dl=0
-//const indirizzoIPDelServer = 'https://andreabrunibrun.github.io/CLIENT-WEB';
+const indirizzoIPDelServer = 'https://andreabrunibrun.github.io/CLIENT-WEB';
 //client-web-odcm-fchm3x6mr-abrunis-projects.vercel.app
 
-const indirizzoIPDelServer = 'https://client-web-odcm.vercel.app';
+//const indirizzoIPDelServer = 'https://client-web-odcm.vercel.app';
 const eventSource = new EventSource(`${indirizzoIPDelServer}/index`);
 
 //const indirizzoIPDelServer = 'http://192.168.1.35:3000';
