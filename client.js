42d86ecd19dc885b6b1a3888b7d3868ad8bf322a
AndreaBruniBRUN <131588971+AndreaBruniBRUN@users.noreply.github.com>
2023-11-06 01:38:28 +0100

    test009

--- client.js
+++ client.js
@@ -10,8 +10,9 @@
 
 //const indirizzoIPDelServer = 'https://client-web-odcm.vercel.app';
 //const eventSource = new EventSource(`${indirizzoIPDelServer}/index`);
-const indirizzoIPDelServer = 'http://192.168.1.35:3000';
-const eventSource = new EventSource(`${indirizzoIPDelServer}/index`);
+//const indirizzoIPDelServer = 'http://192.168.1.35:3000';
+//const eventSource = new EventSource(`${indirizzoIPDelServer}/index`);
+const eventSource = new EventSource('/index');
 
 //const indirizzoIPDelServer = 'http://192.168.1.35:3000';
 //const eventSource = new EventSource(`${indirizzoIPDelServer}/index.html`);
